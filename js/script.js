79836b6489aeabce592502431570dca43b8b0156
sabrina Natasya Bilbina <sabrinatasya170306@gmail.com>
2024-09-09 19:26:44 +0700

    membuat function hitung karakter dan menghutitung nya

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function hitungKarakter (str) {
+    const totalKarakter = str.length;
+
+    const InputKarakter = {};
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        
+    }
+}
